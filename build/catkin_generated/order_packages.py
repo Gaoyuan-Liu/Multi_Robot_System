476a9b062bdd27aa7a72b318a1d3cfa32e16e805
Liu <yuanl0517@gmail.com>
2020-10-13 11:21:24 +0200

    panda works fine.

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = '/home/liu/Multi_Robot_System/src'
-whitelisted_packages = 'hector_quadrotor_controllers'.split(';') if 'hector_quadrotor_controllers' != '' else []
+whitelisted_packages = 'hiro_panda_ik'.split(';') if 'hiro_panda_ik' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
 underlay_workspaces = '/home/liu/Multi_Robot_System/devel;/opt/ros/melodic'.split(';') if '/home/liu/Multi_Robot_System/devel;/opt/ros/melodic' != '' else []
---
+++ build/panda/panda_control/CMakeFiles/panda_positioncontrol_taskspace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/panda_positioncontrol_taskspace.dir/src/panda_positioncontrol_taskspace.cpp.o"
+  "/home/liu/Multi_Robot_System/devel/lib/panda_control/panda_positioncontrol_taskspace.pdb"
+  "/home/liu/Multi_Robot_System/devel/lib/panda_control/panda_positioncontrol_taskspace"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/panda_positioncontrol_taskspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
