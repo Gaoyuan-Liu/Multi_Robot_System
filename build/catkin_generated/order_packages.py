65ab32ca9f640877c8d32234b375892e154c3cb7
Liu <yuanl0517@gmail.com>
2020-11-04 22:17:55 +0100

    fix state publish

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = '/home/liu/Multi_Robot_System/src'
-whitelisted_packages = 'hiro_panda_ik'.split(';') if 'hiro_panda_ik' != '' else []
+whitelisted_packages = 'message_to_tf'.split(';') if 'message_to_tf' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
 underlay_workspaces = '/home/liu/Multi_Robot_System/devel;/opt/ros/melodic'.split(';') if '/home/liu/Multi_Robot_System/devel;/opt/ros/melodic' != '' else []
